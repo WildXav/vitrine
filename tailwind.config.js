9ba2643f67f406bd45158653e95ebb0d0f20905a
Xav <bonjour@xaviers.sh>
2024-01-12 19:58:49 -0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  // eslint-disable-next-line no-undef
+  plugins: [require('daisyui')],
+}
