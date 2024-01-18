bcc1bc50c4b8940ffcacc702ca98644cf88a2010
Xav <bonjour@xaviers.sh>
2024-01-18 18:00:10 -0500

    Automatically resetting trade_data.json when build done to prevent committing trading data

--- parser/plugin.ts
+++ parser/plugin.ts
@@ -9,10 +9,12 @@ export default function ExportParserPlugin(): Plugin {
   return {
     name: 'export-parser',
     buildStart: generateTradeData,
+    buildEnd: resetJsonFile,
   }
 }
 
 async function generateTradeData(): Promise<void> {
+  console.log('\nPnL parser: Generating trade_data.json...')
   const tradesPnl = await parsePnls()
 
   const data: TradeData = {
@@ -22,6 +24,20 @@ async function generateTradeData(): Promise<void> {
       overall: computeStats(tradesPnl),
       monthly: computeStats(tradesPnl, 'M'),
       weekly: computeStats(tradesPnl, 'w'),
+    },
+  }
+  await fs.writeFile('./src/trade_data.json', JSON.stringify(data), 'utf8')
+}
+
+async function resetJsonFile(): Promise<void> {
+  console.log('\nPnL parser: Resetting trade_data.json')
+  const data: TradeData = {
+    updatedAt: 0,
+    profitEvolution: computeEvolution([]),
+    stats: {
+      overall: computeStats([]),
+      monthly: computeStats([]),
+      weekly: computeStats([]),
     },
   }
   await fs.writeFile('./src/trade_data.json', JSON.stringify(data), 'utf8')
